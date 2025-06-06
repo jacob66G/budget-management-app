458773f66f664e7a7570af61c5ed9599c5b100c2
Dvrecky <hubertdurka43@gmail.com>
2025-06-06 19:26:37 +0200

    Initial commit

---
+++ src/main/java/com/example/budget_management_app/BudgetManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.budget_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/budget_management_app/BudgetManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budget_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
