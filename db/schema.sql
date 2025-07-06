afc277a04b52708f7e13c374f272829c25d40a30
JAKUB GERWEL <kuba.gerwel@gmail.com>
2025-07-06 15:48:45 +0200

    update schema sql script

--- db/schema.sql
+++ db/schema.sql
@@ -1,64 +1,176 @@
-CREATE TABLE public."user" (
-    id BIGINT PRIMARY KEY,
-    name TEXT NOT NULL,
-    surname TEXT NOT NULL,
-    email TEXT NOT NULL,
-    password TEXT NOT NULL,
-    email_last_changed TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS public.account
+(
+    id bigint NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    balance numeric(19, 2) NOT NULL,
+    is_default boolean NOT NULL,
+    currency text COLLATE pg_catalog."default" NOT NULL,
+    description text COLLATE pg_catalog."default",
+    created_at timestamp without time zone,
+    user_id bigint,
+    CONSTRAINT account_pkey PRIMARY KEY (id)
+    );
 
-CREATE TABLE public.account (
-    id BIGINT PRIMARY KEY,
-    name TEXT NOT NULL,
-    balance NUMERIC(19,2) NOT NULL,
-    is_default BOOLEAN NOT NULL,
-    currency TEXT NOT NULL,
-    description TEXT,
-    created_at TIMESTAMP NOT NULL,
-    user_id BIGINT,
-    CONSTRAINT account_user_id_fkey FOREIGN KEY (user_id) REFERENCES public."user"(id)
-);
+CREATE TABLE IF NOT EXISTS public.category
+(
+    id bigint NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    type text COLLATE pg_catalog."default" NOT NULL,
+    icon_path text COLLATE pg_catalog."default",
+    is_default boolean NOT NULL,
+    user_id bigint,
+    CONSTRAINT category_pkey PRIMARY KEY (id)
+    );
 
-CREATE TABLE public.category (
-    id BIGINT PRIMARY KEY,
-    name TEXT NOT NULL,
-    type TEXT NOT NULL,
-    color TEXT NOT NULL,
-    icon_path TEXT,
-    is_default BOOLEAN NOT NULL,
-    user_id BIGINT,
-    CONSTRAINT category_user_id_fkey FOREIGN KEY (user_id) REFERENCES public."user"(id)
-);
+CREATE TABLE IF NOT EXISTS public.investment
+(
+    id bigint NOT NULL,
+    investment_account bigint NOT NULL,
+    symbol text COLLATE pg_catalog."default" NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    quantity numeric(20, 6) NOT NULL,
+    avg_buy_price numeric(20, 6) NOT NULL,
+    current_price numeric(20, 6) NOT NULL,
+    currency text COLLATE pg_catalog."default" NOT NULL,
+    unrealized_pl numeric(20, 6),
+    realized_pl numeric(20, 6),
+    manual boolean NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    CONSTRAINT investment_pkey PRIMARY KEY (id)
+    );
 
-CREATE TABLE public.recurring_transaction (
-    id BIGINT PRIMARY KEY,
-    amount NUMERIC(19,2) NOT NULL,
-    title TEXT NOT NULL,
-    description TEXT,
-    start_date TIMESTAMP NOT NULL,
-    end_date TIMESTAMP,
-    frequency TEXT NOT NULL,
-    recurring_value BIGINT,
-    next_occurence TIMESTAMP,
-    is_active BOOLEAN NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    category_id BIGINT,
-    account_id BIGINT,
-    CONSTRAINT recurring_transaction_category_id_fkey FOREIGN KEY (category_id) REFERENCES public.category(id),
-    CONSTRAINT recurring_transaction_account_id_fkey FOREIGN KEY (account_id) REFERENCES public.account(id)
-);
+CREATE TABLE IF NOT EXISTS public.investment_account
+(
+    id bigint NOT NULL,
+    user_id bigint NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    description text COLLATE pg_catalog."default",
+    broker_connected boolean NOT NULL,
+    auth_token text COLLATE pg_catalog."default",
+    last_sync_at timestamp without time zone,
+    created_at timestamp without time zone,
+    CONSTRAINT investment_account_pkey PRIMARY KEY (id)
+    );
 
-CREATE TABLE public.transaction (
-    id BIGINT PRIMARY KEY,
-    amount NUMERIC(19,2) NOT NULL,
-    title TEXT NOT NULL,
-    description TEXT,
-    transaction_date TIMESTAMP NOT NULL,
-    photo_path TEXT,
-    account_id BIGINT,
-    category_id BIGINT,
-    recurring_transaction_id BIGINT,
-    CONSTRAINT transaction_account_id_fkey FOREIGN KEY (account_id) REFERENCES public.account(id),
-    CONSTRAINT transaction_category_id_fkey FOREIGN KEY (category_id) REFERENCES public.category(id),
-    CONSTRAINT transaction_recurring_transaction_id_fkey FOREIGN KEY (recurring_transaction_id) REFERENCES public.recurring_transaction(id)
-);
+CREATE TABLE IF NOT EXISTS public.recurring_transaction
+(
+    id bigint NOT NULL,
+    amount numeric(19, 2) NOT NULL,
+    title text COLLATE pg_catalog."default" NOT NULL,
+    description text COLLATE pg_catalog."default",
+    start_date timestamp without time zone NOT NULL,
+    end_date timestamp without time zone,
+    frequency text COLLATE pg_catalog."default" NOT NULL,
+    recurring_value bigint,
+    next_occurence timestamp without time zone,
+    is_active boolean NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    category_id bigint,
+    account_id bigint,
+    CONSTRAINT recurring_transaction_pkey PRIMARY KEY (id)
+    );
+
+CREATE TABLE IF NOT EXISTS public.transaction
+(
+    id bigint NOT NULL,
+    amount numeric(19, 2) NOT NULL,
+    title text COLLATE pg_catalog."default" NOT NULL,
+    description text COLLATE pg_catalog."default",
+    transaction_date timestamp without time zone NOT NULL,
+    photo_path text COLLATE pg_catalog."default",
+    account_id bigint,
+    category_id bigint,
+    recurring_transaction_id bigint,
+    CONSTRAINT transaction_pkey PRIMARY KEY (id)
+    );
+
+CREATE TABLE IF NOT EXISTS public.transaction_images
+(
+    id bigint NOT NULL,
+    transaction_id bigint NOT NULL,
+    file_name text COLLATE pg_catalog."default" NOT NULL,
+    url text COLLATE pg_catalog."default" NOT NULL,
+    uploaded_at timestamp without time zone,
+    CONSTRAINT transaction_images_pkey PRIMARY KEY (id)
+    );
+
+CREATE TABLE IF NOT EXISTS public."user"
+(
+    id bigint NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    surname text COLLATE pg_catalog."default" NOT NULL,
+    email text COLLATE pg_catalog."default" NOT NULL,
+    password text COLLATE pg_catalog."default" NOT NULL,
+    email_last_changed timestamp without time zone,
+    CONSTRAINT user_pkey PRIMARY KEY (id)
+    );
+
+ALTER TABLE IF EXISTS public.account
+    ADD CONSTRAINT account_user_id_fkey FOREIGN KEY (user_id)
+    REFERENCES public."user" (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS public.category
+    ADD CONSTRAINT category_user_id_fkey FOREIGN KEY (user_id)
+    REFERENCES public."user" (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS public.investment
+    ADD CONSTRAINT investment_investment_account_fkey FOREIGN KEY (investment_account)
+    REFERENCES public.investment_account (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS public.investment_account
+    ADD CONSTRAINT investment_account_user_id_fkey FOREIGN KEY (user_id)
+    REFERENCES public."user" (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS public.recurring_transaction
+    ADD CONSTRAINT recurring_transaction_account_id_fkey FOREIGN KEY (account_id)
+    REFERENCES public.account (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS public.recurring_transaction
+    ADD CONSTRAINT recurring_transaction_category_id_fkey FOREIGN KEY (category_id)
+    REFERENCES public.category (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS public.transaction
+    ADD CONSTRAINT transaction_account_id_fkey FOREIGN KEY (account_id)
+    REFERENCES public.account (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS public.transaction
+    ADD CONSTRAINT transaction_category_id_fkey FOREIGN KEY (category_id)
+    REFERENCES public.category (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS public.transaction
+    ADD CONSTRAINT transaction_recurring_transaction_id_fkey FOREIGN KEY (recurring_transaction_id)
+    REFERENCES public.recurring_transaction (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public.transaction_images
+    ADD CONSTRAINT transaction_images_transaction_id_fkey FOREIGN KEY (transaction_id)
+    REFERENCES public.transaction (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+       ON DELETE NO ACTION;
